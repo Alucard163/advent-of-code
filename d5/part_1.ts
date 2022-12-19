ffb789dafb9d9bee4744584622398f4db01b5e15
Роман <romemix163@gmail.com>
2022-12-19 23:21:44 +0400

    day 5 of advent of code 2022

---
+++ d5/part_1.ts
@@ -0,0 +1,58 @@
+const values = Deno.readTextFileSync('./inputs/day_5.txt').split(
+    '\r\n',
+);
+
+const stacks = {};
+const procedures = [];
+
+values.forEach((line) => {
+    const content = line.replace(/\s/g, '');
+
+    const isUnnecessary = !content || !isNaN(parseInt(content));
+
+    if (isUnnecessary) {
+        return;
+    }
+
+    const isProcedure = line.indexOf('move') > -1;
+
+    if (isProcedure) {
+        const [move, quantity, from, source, to, target] = line.split(
+            ' ',
+        );
+
+        procedures.push({
+            [move]: parseInt(quantity),
+            [from]: parseInt(source),
+            [to]: parseInt(target),
+        });
+    } else {
+        for (let index = 0; index < line.length; index += 4) {
+            const stack = (index / 4) + 1;
+            const crate = line.substring(index, index + 4).match(/\w/g);
+
+            stacks[stack] = (!stacks[stack] ? '' : stacks[stack]) +
+                (crate ? crate : '');
+        }
+    }
+});
+
+procedures.forEach((procedure) => {
+    const cratesRemoved = stacks[procedure.from].substring(
+        0,
+        procedure.move,
+    );
+
+    stacks[procedure.to] =
+        cratesRemoved.split('').reduce((crates, crate) =>
+            crate + crates, '') + stacks[procedure.to];
+    stacks[procedure.from] = stacks[procedure.from].substring(
+        procedure.move,
+    );
+});
+
+const answer = Object.values(stacks).map((stack) => stack[0]).join(
+    '',
+);
+
+console.log(answer);
